67d7cf354fc9a27e9bcfefe9690a7fa8b76b1f07
o0maxwell0o <40237850+o0maxwell0o@users.noreply.github.com>
2020-05-07 22:17:18 +0900

    image upload

--- src/main/java/com/web/vuespring/config/MyWebConfigurer.java
+++ src/main/java/com/web/vuespring/config/MyWebConfigurer.java
@@ -26,5 +26,10 @@ public class MyWebConfigurer implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(getLoginIntercepter()).addPathPatterns("/**").excludePathPatterns("/index.html");
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/file/**").addResourceLocations("file:" + "d:/img/");
+    }
 }
 
--- src/main/java/com/web/vuespring/controller/LibraryController.java
+++ src/main/java/com/web/vuespring/controller/LibraryController.java
@@ -3,9 +3,13 @@ package com.web.vuespring.controller;
 import com.web.vuespring.pojo.Book;
 import com.web.vuespring.service.BookService;
 
+import com.web.vuespring.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -38,4 +42,24 @@ public class LibraryController {
             return list();
         }
     }
+
+    @CrossOrigin
+    @PostMapping("/api/covers")
+    public String coversUpload(MultipartFile file) throws Exception {
+        String folder = "D:/img/";
+        File imageFolder = new File(folder);
+        File f = new File(imageFolder, StringUtils.getRandomString(6) + file.getOriginalFilename()
+                .substring(file.getOriginalFilename().length() - 4));
+        if (!f.getParentFile().exists())
+            f.getParentFile().mkdirs();
+        try {
+            file.transferTo(f);
+            String imgURL = "http://localhost:8443/api/file/" + f.getName();
+            return imgURL;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
 }
